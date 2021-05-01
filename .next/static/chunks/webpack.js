0eedd1b4315711b2ad375d52df84ce64b4954903
jasonxuyang <jasonxuyang@gmail.com>
2021-05-01 09:38:38 -0400

    added transitions to mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "95365a84642298fb8544";
+/******/ 	var hotCurrentHash = "60020ca7eeb082f5fabe";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
