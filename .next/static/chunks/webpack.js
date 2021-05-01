8cb92e553e6b7164d6a191efd228afb3117a74dd
jasonxuyang <jasonxuyang@gmail.com>
2021-05-01 08:55:32 -0400

    fixed thumbnail mobile responsiveness

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d739ffdf0bca01a8c071";
+/******/ 	var hotCurrentHash = "3c91c0bf51302b69489b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
